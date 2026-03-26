13ebf481573b9d8e1925ccb40a6eba161c458f2e
Jaheim Thompson <113160062+JaheimT@users.noreply.github.com>
2026-03-26 10:21:29 +0000

    Add files via upload

--- Password Checker.py
+++ Password Checker.py
@@ -3,6 +3,12 @@ password = (input("enter your password: "))
 # checks length of password
 if 8 <= len(password)<= 16:
     print("password accepted")
+    if str.isdigit(password) == True:
+        print("password accepted")
+
+
+    else:
+        print("password must contain numbers")
 else:
     print("password has to be between 8 - 12 characters")
 
