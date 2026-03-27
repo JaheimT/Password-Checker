4ce28751b2c5491fd291b355ac991ea6b62cf95a
Jaheim Thompson <113160062+JaheimT@users.noreply.github.com>
2026-03-27 11:35:11 +0000

    Fixed how the code found numbers in the password

--- Password Checker.py
+++ Password Checker.py
@@ -1,16 +1,16 @@
 # user inputs password
 password = (input("enter your password: "))
 # checks length of password
-if 8 <= len(password)<= 16:
+if 8 <= len(password) <= 16:
     print("password accepted")
-    if str.isdigit(password) == True:
+    if any(char.isdigit() for char in password):
         print("password accepted")
+        if any(char.isupper() for char in password):
+            print("password accepted")
 
-
+        else:
+            print("password must contain uppercase charecters")
     else:
         print("password must contain numbers")
 else:
     print("password has to be between 8 - 12 characters")
-
-
-
